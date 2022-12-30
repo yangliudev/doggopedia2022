67e351dabafd08f2dab56210a4c6b0e60c22908a
Yang Liu <ylyangliu1@gmail.com>
2022-12-30 12:40:59 -0800

    move content

--- screens/HomeScreen.tsx
+++ screens/HomeScreen.tsx
@@ -10,33 +10,33 @@ const HomeScreen = () => {
   //   const dispatch = useDispatch()<any>;
   //   dispatch(apiRequest('Craig%20Noone'));
 
-  const testGET = () => {
-    axios
-      .get(
-        'https://en.wikipedia.org/w/api.php?format=json&action=query&prop=extracts&exintro&explaintext&redirects=1&titles=Stack%20Overflow',
-      )
-      .then(response => {
-        let responseData = response.data.query.pages;
-        console.log('test get ', responseData);
-      })
-      .catch(error => {
-        console.log('apiRequest.js error is ', error);
-      });
-  };
+  // const testGET = () => {
+  //   axios
+  //     .get(
+  //       'https://en.wikipedia.org/w/api.php?format=json&action=query&prop=extracts&exintro&explaintext&redirects=1&titles=Stack%20Overflow',
+  //     )
+  //     .then(response => {
+  //       let responseData = response.data.query.pages;
+  //       console.log('test get ', responseData);
+  //     })
+  //     .catch(error => {
+  //       console.log('apiRequest.js error is ', error);
+  //     });
+  // };
 
-  const testGETDog = () => {
-    axios
-      .get(
-        'https://en.wikipedia.org/w/api.php?format=json&action=query&prop=extracts&exintro&explaintext&redirects=1&titles=German_Hound',
-      )
-      .then(response => {
-        let responseData = response.data.query.pages;
-        console.log('test get dog ', responseData);
-      })
-      .catch(error => {
-        console.log('apiRequest.js error is ', error);
-      });
-  };
+  // const testGETDog = () => {
+  //   axios
+  //     .get(
+  //       'https://en.wikipedia.org/w/api.php?format=json&action=query&prop=extracts&exintro&explaintext&redirects=1&titles=German_Hound',
+  //     )
+  //     .then(response => {
+  //       let responseData = response.data.query.pages;
+  //       console.log('test get dog ', responseData);
+  //     })
+  //     .catch(error => {
+  //       console.log('apiRequest.js error is ', error);
+  //     });
+  // };
 
   return <Text>hello</Text>;
 };
--- screens/QuizScreen.tsx
+++ screens/QuizScreen.tsx
@@ -1,16 +1,19 @@
 import React, {useEffect, useState} from 'react';
 import styled from 'styled-components/native';
-import {Text, StyleSheet, Image, Button} from 'react-native';
+import {Text, StyleSheet, Image, Button, TextInput} from 'react-native';
 
 import axios from 'axios';
 
 const QuizScreen = () => {
   const [dogImgUrlLink, setDogImgUrlLink] = useState('');
+  const [dogName, setDogName] = useState('');
+
+  const [userInput, setUserInput] = useState('');
 
   useEffect(() => {
     getRandomDogImage();
     return () => {
-      console.log('getRandomDogImage() cleanup');
+      console.log('QuizScreen.tsx cleanup');
     };
   }, []);
 
@@ -19,12 +22,45 @@ const QuizScreen = () => {
       .get('https://dog.ceo/api/breeds/image/random')
       .then(response => {
         let responseData = response.data;
-        setDogImgUrlLink(responseData.message);
+        findDogName(responseData.message);
+
         console.log('responseData ', responseData);
       })
       .catch(error => {
-        console.log('apiRequest.js error is ', error);
+        console.log('getRandomDogImage() error is ', error);
       });
+  };
+
+  const findDogName = (responseString: any) => {
+    setDogImgUrlLink(responseString);
+
+    let startIndex;
+    let endIndex;
+
+    let count = 0;
+    for (let i = 0; i < responseString.length; i++) {
+      if (responseString[i] === '/' && count !== 4) {
+        count++;
+        i++;
+        startIndex = i++;
+      } else {
+        if (responseString[i] === '/') {
+          endIndex = i;
+          break;
+        }
+        i++;
+      }
+    }
+    let producedString = responseString.substr(startIndex, endIndex);
+    console.log('STRING IS ', producedString);
+  };
+
+  const handleUserInput = (input: any) => {
+    setUserInput(input);
+  };
+
+  const checkInputGuess = () => {
+    console.log('user input is ', userInput);
   };
 
   return (
@@ -37,6 +73,10 @@ const QuizScreen = () => {
         }}
       />
       <Button title="Shuffle doggo" onPress={() => getRandomDogImage()} />
+
+      <TextInputStyled onChangeText={text => handleUserInput(text)} />
+
+      <Button title="Check" onPress={() => checkInputGuess()} />
     </Container>
   );
 };
@@ -49,6 +89,13 @@ const Container = styled.View`
 
 const Header = styled.Text`
   font-size: 30px;
+`;
+
+const TextInputStyled = styled.TextInput`
+  width: 150px;
+  height: 40px;
+  border-radius: 5px;
+  background-color: #c5ddfa;
 `;
 
 const styles = StyleSheet.create({
