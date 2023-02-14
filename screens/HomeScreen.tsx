18c256d45f155beb8cc69b9b66413941aabd2a01
Yang Liu <ylyangliu1@gmail.com>
2023-02-14 15:38:06 -0800

    add dropdown menu library

--- screens/HomeScreen.tsx
+++ screens/HomeScreen.tsx
@@ -1,27 +1,49 @@
 import React, {useEffect, useState} from 'react';
-import {View, Text, Image, StyleSheet, Button} from 'react-native';
+import {ScrollView, View, Text, Image, StyleSheet, Button} from 'react-native';
+import {SelectList} from 'react-native-dropdown-select-list';
 
 import {useDispatch} from 'react-redux';
 import {apiRequest} from '../redux/slices/apiRequest';
 // import instance from '../utilities/axios';
 import axios from 'axios';
+import styled from 'styled-components';
 
 const HomeScreen = () => {
   const dispatch = useDispatch()<any>;
   dispatch(apiRequest('Craig%20Noone'));
 
+  // JSON data scraped off of Wikipedia's Dog Breeds List
+  let wikiJsonObj = require('../api/cleanedData.json');
+  let wikiJsonString = wikiJsonObj[0].dogBreeds;
+  let jsonDataArray = wikiJsonString.split(',');
+
   useEffect(() => {
     testGET();
-    testGETDog();
     return () => {
       console.log('clean up');
     };
   }, []);
 
+  const [selected, setSelected] = useState('');
+
+  const data = [
+    {key: '1', value: 'Mobiles'},
+    {key: '2', value: 'Appliances'},
+    {key: '3', value: 'Cameras'},
+    {key: '4', value: 'Computers'},
+    {key: '5', value: 'Vegetables'},
+    {key: '6', value: 'Diary Products'},
+    {key: '7', value: 'Drinks'},
+  ];
+
+  const evalSelectedText = dogName => {
+    console.log('SELECTED IS ', dogName);
+  };
+
   const testGET = () => {
     axios
       .get(
-        'https://en.wikipedia.org/w/api.php?format=json&action=query&prop=extracts&exintro&explaintext&redirects=1&titles=Stack%20Overflow',
+        'https://en.wikipedia.org/w/api.php?format=json&action=query&prop=extracts&exintro&explaintext&redirects=1&titles=American Bully',
       )
       .then(response => {
         let responseData = response.data.query.pages;
@@ -32,21 +54,32 @@ const HomeScreen = () => {
       });
   };
 
-  const testGETDog = () => {
-    axios
-      .get(
-        'https://en.wikipedia.org/w/api.php?format=json&action=query&prop=extracts&exintro&explaintext&redirects=1&titles=German_Hound',
-      )
-      .then(response => {
-        let responseData = response.data.query.pages;
-        console.log('test get dog ', responseData);
-      })
-      .catch(error => {
-        console.log('apiRequest.js error is ', error);
-      });
-  };
-
-  return <Text>hello</Text>;
+  return (
+    <ScrollView>
+      <View style={styles.container}>
+        <Text>Select a dog breed to learn more!</Text>
+        {/* <Text>{wikiJsonString}</Text> */}
+        <SelectList
+          setSelected={val => setSelected(val)}
+          data={data}
+          save="value"
+          onSelect={() => evalSelectedText(selected)}
+        />
+      </View>
+    </ScrollView>
+  );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 20,
+  },
+});
+
+// const Container = styled(View)`
+//   flex: 1,
+//   padding: 20,
+// `;
 
 export default HomeScreen;
